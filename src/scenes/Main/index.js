d2164a902771247b129ed50074e82fe886ef7cba
Damian Sznajder <damsznajder@gmail.com>
2018-09-11 22:26:03 +0200

    Setup eslint for more helpful imports

--- src/scenes/Main/index.js
+++ src/scenes/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
-import { white, black } from 'src/styles/colors'
+
+import { black, white } from 'src/styles/colors'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
