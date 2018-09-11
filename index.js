90ae7c0c1cb326f6ff980d312c09cea14760159a
Damian Sznajder <damsznajder@gmail.com>
2018-09-11 22:28:02 +0200

    :fire: Remove debugging code

--- index.js
+++ index.js
@@ -3,12 +3,4 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 
-if (__DEV__) {
-  /* eslint-disable */
-  XMLHttpRequest = GLOBAL.originalXMLHttpRequest
-    ? GLOBAL.originalXMLHttpRequest
-    : GLOBAL.XMLHttpRequest
-  /* eslint-enable */
-}
-
 AppRegistry.registerComponent(appName, () => App)
