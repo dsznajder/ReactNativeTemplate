9597f20969c032b28622c3a78c90e5efd3a7a208
Damian Sznajder <damsznajder@gmail.com>
2018-12-24 14:14:31 +0100

    :star: Add react-native-screens

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { createStackNavigator } from 'react-navigation'
+import { useScreens } from 'react-native-screens'
 
 import Main from 'src/scenes/Main'
+useScreens()
 
 const Scenes = {
   Main,
