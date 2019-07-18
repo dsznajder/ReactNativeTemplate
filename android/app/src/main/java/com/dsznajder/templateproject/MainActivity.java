fdc7d21f2b8bdac4c287cdb440d0febaf7ff86a0
Damian Sznajder <damsznajder@gmail.com>
2019-07-18 22:17:44 +0200

    Update MainActivity to not use ReactFragmentActivity

--- android/app/src/main/java/com/dsznajder/templateproject/MainActivity.java
+++ android/app/src/main/java/com/dsznajder/templateproject/MainActivity.java
@@ -1,19 +1,13 @@
 package com.dsznajder.templateproject;
 
-import android.os.Bundle;
-import com.facebook.react.ReactFragmentActivity;
+import android.content.Intent;
+
+import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
-
-public class MainActivity extends ReactFragmentActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(null);
-    }
-
+public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "TemplateProject";
@@ -21,11 +15,11 @@ public class MainActivity extends ReactFragmentActivity {
 
     @Override
     protected ReactActivityDelegate createReactActivityDelegate() {
-      return new ReactActivityDelegate(this, getMainComponentName()) {
-        @Override
-        protected ReactRootView createRootView() {
-         return new RNGestureHandlerEnabledRootView(MainActivity.this);
-        }
-      };
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
     }
 }
