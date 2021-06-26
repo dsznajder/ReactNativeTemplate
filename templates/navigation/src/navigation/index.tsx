7e1d95b34fcc287988e6914110178025a3ed59e7
Daniel Szczepanik <tmlm18@gmail.com>
2021-06-26 16:07:59 +0200

    Set up basic Navigation screen

--- templates/navigation/src/navigation/index.tsx
+++ templates/navigation/src/navigation/index.tsx
@@ -1,37 +1,58 @@
-/**
- * If you are not familiar with React Navigation, check out the "Fundamentals" guide:
- * https://reactnavigation.org/docs/getting-started
- *
- */
-import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
 
-import NotFoundScreen from '../screens/NotFoundScreen';
-import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
-import LinkingConfiguration from './LinkingConfiguration';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
-  return (
-    <NavigationContainer
-      linking={LinkingConfiguration}
-      theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <RootNavigator />
-    </NavigationContainer>
-  );
-}
+import HomeScreen from '~/screens/Home';
+import SettingsScreen from '~/screens/Settings';
 
-// A root stack navigator is often used for displaying modals on top of all other content
-// Read more here: https://reactnavigation.org/docs/modal
+import palette from '~/styles/palette';
+
+export type RootStackParamList = {
+  Home: undefined;
+  Settings: undefined;
+};
+
+const screenOptions = {
+  headerShown: false,
+};
+
+<% if (modules.navigation.isStack) { %>
 const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
-      <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
     </Stack.Navigator>
+  );
+}
+<% } else { %>
+// Configure bottom tab navigator
+  
+const Stack = createStackNavigator<RootStackParamList>();
+
+function RootNavigator() {
+  return (
+    <Stack.Navigator screenOptions={screenOptions}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+<% } %>
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor={palette.common.black}
+      />
+
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
--- templates/navigation/src/screens/NotFoundScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { memo } from 'react';
-
-import Typography from '~/components/Typography';
-
-const NotFoundScreen = () => {
-  return <Typography>We couldn't find this URL</Typography>;
-};
-
-export default memo(NotFoundScreen);
