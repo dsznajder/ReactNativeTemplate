1bcf27ef604e81d4096662e3283bdc1b184bc88d
Daniel Szczepanik <tmlm18@gmail.com>
2021-07-05 21:13:38 +0200

    Remove git helpers, which were executed too often

--- templates/common/scripts/postinstall.sh
+++ templates/common/scripts/postinstall.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-git init;
-
 yarn husky install;
 yarn jetify;
 
@@ -9,5 +7,3 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   echo "Installing pods..."
   cd ios && pod install --repo-update && cd ..;
 fi
-
-git add . && git commit -m 'Initial commit';
